14b4175979336c6664230d056fbbb286584f344d
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2020-07-26 15:17:00 -0300

    chore: libs fixes

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/**/*-protocols.ts",
+    "!**/protocols/**",
+    "!**/test/**",
+  ],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
   transform: {
     ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
   },
 };
