8a9d8dfb4195dfa5938b4733be5024407350be40
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2020-08-15 14:53:08 -0300

    feat: cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,9 @@
 import { Express } from 'express'
+
 import { bodyParser } from '../middlewares/body-parser'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
+  app.use(cors)
   app.use(bodyParser)
 }
