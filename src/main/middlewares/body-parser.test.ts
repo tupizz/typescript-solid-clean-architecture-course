659fb0851dab4a3ad45b9c5337327a9112d6ece1
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2020-08-15 14:43:52 -0300

    feat: middlewares

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body parser middleware', () => {
+  test('should parse body as json with body parser of express', async () => {
+    app.post('/test-body-parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test-body-parser')
+      .send({ name: 'Tadeu' })
+      .expect({ name: 'Tadeu' })
+  })
+})
---
+++ src/main/server.ts
@@ -0,0 +1,5 @@
+import app from './config/app'
+
+app.listen(5050, () =>
+  console.log('🚀 Server running at http://localhost:5050')
+)
