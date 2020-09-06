8517b9bf77fe4753361527d9b85fe42cf45dfa78
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2020-09-05 23:58:28 -0300

    feat: factory with correct values

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -5,6 +5,8 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { LogControllerDecorator } from '../decorators/log'
 import { Controller } from '../../presentation/protocols'
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorMongoRepository } from '../../infra/db/mongodb/log-error-repository/log-error'
 
 const makeAddAccountUseCase = (): DbAddAccount => {
   const encrypter = new BcryptAdapter(12)
@@ -16,9 +18,13 @@ const makeEmailValidator = (): EmailValidatorAdapter => {
   return new EmailValidatorAdapter()
 }
 
+const makeLogRepository = (): LogErrorRepository => {
+  return new LogErrorMongoRepository()
+}
+
 const makeSignUpController = (): Controller => {
   const controller = new SignUpController(makeEmailValidator(), makeAddAccountUseCase())
-  return new LogControllerDecorator(controller)
+  return new LogControllerDecorator(controller, makeLogRepository())
 }
 
 export { makeSignUpController }
